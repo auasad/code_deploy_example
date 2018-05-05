99f485cba546178352753876ceeeabbdeabfd1bc
auasad <36460334+auasad@users.noreply.github.com>
2018-05-05 20:22:21 +0500

    wrong location

--- afterInstall.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# I have left a few things in here and will explain this further (see below)
-rsync --delete-before --verbose --archive --exclude "htdocs/media/" --exclude ".*" --exclude "htdocs/var/" --exclude "htdocs/app/etc/local.xml" /var/www/release/ /var/www/magento/ > /var/log/deploy.log
